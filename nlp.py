502415cfc6d2eabb33fd7d32c68bae81b66a244f
EricFan24 <fanxiaoyuan2015@outlook.com>
2021-11-08 14:24:06 -0500

    implement extract keywords

--- nlp.py
+++ nlp.py
@@ -11,10 +11,27 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
 
-# predict category from string
-def predict_category(text: str) -> str:
-    pass
+class nlp:
+    def __init__(self, articles: list):
+        self.articles = articles
+        self.en_core_web_sm = spacy.load('en_core_web_sm')
+        self.keywords = []
+        self.categories = []
 
-# extract keywords from string
-def extract_keywords(text: str) -> list:
-    pass
+    # extract list of keywords from string
+    def extract_keywords(self) -> list:
+        # lemmatize using spacy
+        articles = [" ".join([token.lemma_ for token in self.en_core_web_sm(article)]) for article in self.articles]
+
+        # remove stop words
+        articles = [utils.simple_preprocess(article) for article in articles]
+        wordsToRemove = ['pron', '']
+        self.keywords = [[remove_stopwords(word) for word in article if remove_stopwords(word) not in wordsToRemove] for article in articles]
+
+        return self.keywords
+
+    # predict news category from string, using trained model
+    def predict_categories(self) -> list:
+        pass
+
+print(nlp(["Democrats, Stung by Electoral Losses, Press Forward on Biden Agenda", "Young Children Are Lining Up for Next Wave of Covid Vaccines"]).extract_keywords())
