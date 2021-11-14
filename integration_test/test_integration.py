23f3f941cd9de777b8b6bebbbe177d257119542f
AlexandraCheng <48686068+AlexandraCheng@users.noreply.github.com>
2021-11-13 22:22:50 -0500

    Create test_integration.py

---
+++ integration_test/test_integration.py
@@ -0,0 +1,14 @@
+import unittest
+from NLP import NLP
+from web_scraper import Scraper
+from main_api import BookmarkTagger
+import db
+
+class Test_good_data(unittest.TestCase):
+  pass
+
+class Test_bad_data(unittest.TestCase):
+  pass
+
+if __name__ == '__main__':
+    unittest.main()
