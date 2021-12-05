1c9621fb2eae473ca3072be250e36cb3a5eed637
AlexandraCheng <48686068+AlexandraCheng@users.noreply.github.com>
2021-12-05 15:37:19 -0500

    authentication

--- authcheck.py
+++ authcheck.py
@@ -3,26 +3,17 @@ from six.moves.urllib.request import urlopen
 from functools import wraps
 
 from flask import Flask, request, jsonify, _request_ctx_stack
-from flask_cors import cross_origin
 from jose import jwt
 
-AUTH0_DOMAIN = 'https://dev-2ajo016m.us.auth0.com/'
+AUTH0_DOMAIN = 'dev-2ajo016m.us.auth0.com'
 API_AUDIENCE = 'https://smart_bookmarks/api'
 ALGORITHMS = ["RS256"]
-
-APP = Flask(__name__)
 
 # Error handler
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
-
-@APP.errorhandler(AuthError)
-def handle_auth_error(ex):
-    response = jsonify(ex.error)
-    response.status_code = ex.status_code
-    return response
 
 
 # Format error response and append status code
--- main.py
+++ main.py
@@ -2,15 +2,41 @@
 This module handles the incoming requests to the Bookmark tagging service.
 """
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_restful import Resource, Api
+from flask_cors import cross_origin
+from jose import jwt
+from werkzeug.wrappers import response
 
 from web_scraper import Scraper
 from nlp import NLP
 import db
+import authcheck
 
 app = Flask(__name__)
 api = Api(app)
+
+
+# This doesn't need authentication
+# simply send a GET request to the route to test access
+@app.route("/api/public")
+@cross_origin(headers=["Content-Type", "Authorization"])
+def public():
+    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+    return jsonify(message=response)
+
+
+# This needs authentication
+# add an entry in Postman header with
+# key = 'Authorization'
+# value = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IktjN3RXYi1udHBYdDZ6SkRpYXJrTyJ9.eyJpc3MiOiJodHRwczovL2Rldi0yYWpvMDE2bS51cy5hdXRoMC5jb20vIiwic3ViIjoidmxLbkZjTFdIaEExNlY2RFVaYmNPelh5R2xmVnVYTjBAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc21hcnRfYm9va21hcmtzL2FwaSIsImlhdCI6MTYzODczMTYyNSwiZXhwIjoxNjM4ODE4MDI1LCJhenAiOiJ2bEtuRmNMV0hoQTE2VjZEVVpiY096WHlHbGZWdVhOMCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.T1mkPwDKkNh19Je15VQ5WAL1GWqzjKedmWN1U_knG7GaRbwZvU8PU-xdVolnHJG0V034G3ONbGpkXf07N2s6N7iZBnguOuE1V_xKLAv7PL2eIMIN863-Fjc4QuoxDNQzIMP4KgKJaxgZHO4hezdfDh5r8PAIsk8JPWGT7zSuHHxJSkHq8qoxqHUoXTfd7ui3Z3vcoXZ78h9xsrvPTWcEEtoK9xHsCk-SZ6ZCj3JCJYcBTnDmkphX_xEPQ60jvli_2_g_SIoYJQFgwpdaCDYL1mri1j1v53gg8oV5-TAQb5xBoma5iOd6m2MYcDYY-fS1ZQV4ysY2SNkIkc6o7I8W6g'
+# and send a GET request to test the access
+@app.route("/api/private")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@authcheck.requires_auth
+def private():
+    response = "Hello from a private endpoint! You need to be authenticated to see this."
+    return jsonify(message=response)  
 
 
 @app.route('/', methods=['GET'])
@@ -26,6 +52,9 @@ class BookmarkTagger(Resource):
     def __init__(self):
         db.init_db()
 
+
+    @authcheck.requires_auth
+    @cross_origin(headers=["Content-Type", "Authorization"])
     def get(self):
         '''
         Search bookmarks using tags.
@@ -51,6 +80,9 @@ class BookmarkTagger(Resource):
 
         return {'urls': common_urls}, 200  # return data with 200 OK
 
+
+    @authcheck.requires_auth
+    @cross_origin(headers=["Content-Type", "Authorization"])
     def post(self):
         '''
         Handles tagging and adding a new bookmark to the database.
@@ -85,6 +117,28 @@ class BookmarkTagger(Resource):
 
 
 api.add_resource(BookmarkTagger, '/tags')  # add endpoint
+
+
+@app.errorhandler(authcheck.AuthError)
+def handle_auth_error(ex):
+    """
+    Handles authorization related errors
+    """
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+@app.errorhandler(jwt.JWTError)
+def handle_jwt_error(ex):
+    """
+    Handles JWT related errors
+    """
+    response = jsonify({'code': 'invalid_header', 
+        'description': 'Error decoding token headers'})
+    response.status_code = 401
+
+    return response
 
 if __name__ == '__main__':
     app.run()
