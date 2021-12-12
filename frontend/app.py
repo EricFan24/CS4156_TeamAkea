6489665074dc2c290cc65cf314bcf4998a23cb57
adit-10 <80546292+adit-10@users.noreply.github.com>
2021-12-11 23:16:01 -0500

    Stlye fixes in frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,37 +1,26 @@
-import re
-from flask import Flask, render_template, request, jsonify, session
-# from json import dump
+'''Flask app for client'''
 import os
-import requests
-from requests.api import head
-from werkzeug.utils import redirect
-import http.client
+from flask import Flask, render_template, request
 import requests
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-
-'''
-{
-      "urls": ["https://www.usatoday.com/story/news/nation/2021/11/10/atmospheric-river-wallop-pacific-northwest/6370849001/",
-    "https://www.nbcnews.com/news/us-news/ability-force-recalls-fda-can-only-warn-consumers-benzene-hand-sanitiz-rcna4585",
-    "https://www.nytimes.com/2021/11/10/climate/climate-cop26-glasgow.html"]
-}
-
-'''
-
-SERVERURI =  "http://127.0.0.1:5000/"  
+SERVERURI =  "http://127.0.0.1:5000/"
 dicty={'access_token':"hello"}
 
 @app.route('/')
-def player1_connect():
+def render_login():
+
+    '''Render login page'''
+
     return render_template('login.html')
 
 @app.route('/tag_urls')
 def tag_urls():
-    
+
+    '''Generate tags for the URLs'''
+
     urls_string = request.args['urls']
     tags_string = request.args['tags'].strip()
 
@@ -46,22 +35,24 @@ def tag_urls():
     print(headers)
 
     if len(tags_string)==0:
-        response = requests.post(SERVERURI+ "/tags", json={"urls": urls, "user_id": user_id}, headers=headers)   
-        print(response.json()) 
+        response = requests.post(SERVERURI+ "/tags", \
+            json={"urls": urls, "user_id": user_id}, headers=headers)
+        print(response.json())
 
     else:
-        response = requests.post(SERVERURI+ "/tags", json={"urls": urls, "user_id": user_id, "tags": tags}, headers=headers)   
+        response = requests.post(SERVERURI+ "/tags", \
+            json={"urls": urls, "user_id": user_id, "tags": tags}, headers=headers)
         print(response.json())
 
     returned_tags = response.json()['tags']
     context=dict(stream="Tags", data=returned_tags)
     return render_template("results.html", **context)
-    #return render_template("index.html")
 
 @app.route('/search_urls')
-def tag_url():
-    
-    #urls_string = request.args['urls']
+def search_url():
+
+    '''Search URLs using tags'''
+
     tags_string = request.args['tags']
 
     access_token = dicty['access_token']
@@ -69,20 +60,22 @@ def tag_url():
         'authorization' : "Bearer " + access_token
     }
     user_id = dicty['user_id']
-    #urls = [x.strip() for x in urls_string.split(',')]
     tags = [x.strip() for x in tags_string.split(',')]
 
-    response = requests.get(SERVERURI+ "/tags", json={"tags": tags,  "user_id": user_id}, headers=headers)
+    response = requests.get(SERVERURI+ "/tags", \
+        json={"tags": tags,  "user_id": user_id}, headers=headers)
     print(response.json())
 
     returned_urls = response.json()["urls"]
 
     context=dict(stream="URLs", data=returned_urls)
-    return render_template("results.html", **context)    
+    return render_template("results.html", **context)
 
 @app.route('/modify_tags')
 def modify_tags():
-    
+
+    '''Modify tags'''
+
     url = request.args['url'].strip()
     tags_to_add_string = request.args['tags_to_add'].strip()
     tags_to_remove_string = request.args['tags_to_remove'].strip()
@@ -95,20 +88,26 @@ def modify_tags():
     headers = {
         'authorization' : "Bearer " + access_token
     }
-    response = requests.post(SERVERURI+ "/edit-tags", json={"user_id": user_id, "url": url, "tags_to_add": tags_to_add, "tags_to_remove": tags_to_remove}, headers=headers)
+    response = requests.post(SERVERURI+ "/edit-tags", \
+        json={"user_id": user_id, "url": url, "tags_to_add": tags_to_add, \
+            "tags_to_remove": tags_to_remove}, headers=headers)
     print("Response JSON:", response.json())
     context=dict(stream="Tags after editing", data=response.json()["tags"])
     return render_template("results.html", **context)
 
 @app.route('/similar_urls')
 def similar_urls():
+
+    '''Find similar URLs'''
+
     url = request.args['url'].strip()
     access_token = dicty['access_token']
     user_id = dicty['user_id']
     headers = {
         'authorization' : "Bearer " + access_token
     }
-    response = requests.get(SERVERURI+ "/similar_urls", json={"url": url,  "user_id": user_id}, headers=headers)
+    response = requests.get(SERVERURI+ "/similar_urls", \
+        json={"url": url,  "user_id": user_id}, headers=headers)
     returned_urls = response.json()["urls"]
     context=dict(stream="URLS", data=returned_urls)
     return render_template("results.html", **context)
@@ -116,7 +115,9 @@ def similar_urls():
 
 @app.route('/show_tags')
 def show_tags():
-    
+
+    '''Get tags for specified URLs'''
+
     urls_string = request.args['urls']
 
     urls = [x.strip() for x in urls_string.split(',')]
@@ -128,68 +129,62 @@ def show_tags():
     }
     print(headers)
 
-    
-    response = requests.get(SERVERURI+ "/get-tags", json={"urls": urls, "user_id": user_id}, headers=headers)   
-    print(response.json()) 
+    response = requests.get(SERVERURI+ "/get-tags", \
+        json={"urls": urls, "user_id": user_id}, headers=headers)
+    print(response.json())
 
     returned_tags = []
     tag_dict = response.json()['tags']
 
-    for u in tag_dict:
-        returned_tags.append(u+ ": ")
-        returned_tags.append(tag_dict[u])
+    for url in tag_dict:
+        returned_tags.append(url+ ": ")
+        returned_tags.append(tag_dict[url])
     context=dict(stream="Tags", data=returned_tags)
     return render_template("results.html", **context)
 
 @app.route('/redirect')
 def redirect():
-    return render_template("index.html")
 
+    ''' Redirect'''
+
+    return render_template("index.html")
 
 @app.route('/user_login', methods=["POST"])
 def user_login():
-  user_id=request.form['user_id']
-  password=request.form['psw']
 
-  payload = {"user_id": user_id, "password": password}
-  print(payload)
+    '''USer login'''
 
-  r = requests.get(SERVERURI + "user-check", json=payload)
-  dicty['access_token'] = r.json()['access_token']
-  dicty['user_id'] = user_id
-  
-  print(r.json())  
-  
-  return render_template("index.html")
+    user_id=request.form['user_id']
+    password=request.form['psw']
+
+    payload = {"user_id": user_id, "password": password}
+    print(payload)
+
+    resp = requests.get(SERVERURI + "user-check", json=payload)
+    dicty['access_token'] = resp.json()['access_token']
+    dicty['user_id'] = user_id
+    print(resp.json())
+    return render_template("index.html")
 
 @app.route('/user_sign_up', methods=["POST"])
 def user_sign_up():
-  user_id=request.form['user_id']
-  password=request.form['psw']
 
-  payload = {"user_id": user_id, "password": password}
-  print(payload)
+    '''New User Sign Up'''
 
-  r = requests.post(SERVERURI + "add-user", json=payload)
-  
-  r = requests.get(SERVERURI + "user-check", json=payload)
-  
-  dicty['access_token'] = r.json()['access_token']
-  dicty['user_id'] = user_id
-  
-  print(r.json())  
-  
-  return render_template("index.html")
-  '''
-  if not rows:
-    print("Login Failed, Please check the login details")
-    cursor.close()
-    return render_template("login.html")
-  else:
-    u_id=rows[0][0]
-    cursor.close()
-    return redirect(('user/{}'.format(u_id)))
-  '''
+    user_id=request.form['user_id']
+    password=request.form['psw']
+
+    payload = {"user_id": user_id, "password": password}
+    print(payload)
+
+    resp = requests.post(SERVERURI + "add-user", json=payload)
+    resp = requests.get(SERVERURI + "user-check", json=payload)
+    dicty['access_token'] = resp.json()['access_token']
+    dicty['user_id'] = user_id
+
+    print(resp.json())
+
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8111)
